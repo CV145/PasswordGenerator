dda80b7ee6d643b8ded815d8de9cf156db5a8dee
Carlos <carlosvaleriano45@yahoo.com>
2023-03-11 17:43:43 -0600

    Modifying css

--- PasswordGenerator/Pages/Index.cshtml.cs
+++ PasswordGenerator/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Windows;
 
 namespace PasswordGenerator.Pages
 {
