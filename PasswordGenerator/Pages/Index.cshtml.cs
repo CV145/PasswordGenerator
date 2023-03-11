2c074e4dd1da6f3e4734dd4bfabb41a2f8e77fa8
Carlos <carlosvaleriano45@yahoo.com>
2023-03-11 12:34:05 -0600

    Password generator portfolio project custom c# logic

--- PasswordGenerator/Pages/Index.cshtml.cs
+++ PasswordGenerator/Pages/Index.cshtml.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace PasswordGenerator.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [Range(8, 12,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int PasswordLength { get; set; } = 8;
+        public bool IncludeUppercase { get; set; } = true;
+        public bool IncludeLowercase { get; set; }
+        = true;
+        public bool IncludeNumbers { get; set; }
+       = true;
+        public bool IncludeSymbols { get; set; }
+       = true;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
